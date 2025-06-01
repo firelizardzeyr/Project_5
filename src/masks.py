f2dc3644bbd277915b7b9d615b1ed1b4d2af23e6
Denis Adler <firelizard@mail.ru>
2025-06-01 15:56:27 +0700

    коммит начала работ

---
+++ src/masks.py
@@ -0,0 +1,23 @@
+"""Этот модуль содержит функции, возвращающие маски номеров счета
+и карты"""
+
+
+def get_mask_card_number(card_number: int) -> str:
+    """Принимает номер карты int, выдаёт замаскированный str вида
+    ХХХХ ХХ** **** ХХХХ"""
+
+    new_mask: str = (
+        f"{str(card_number)[:4]} {str(card_number)[4:6]}** **** \
+{str(card_number)[8:12]}"
+    )
+
+    return new_mask
+
+
+def get_mask_account(account_number: int) -> str:
+    """Принимает номер аккаунта int, выдаёт замаскированный" str вида
+    **ХХХХ"""
+
+    new_mask: str = f"**{str(account_number)[-5:-1]}"
+
+    return new_mask
