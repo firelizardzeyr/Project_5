cae54b9012eae0916e0954d961b637220f12c6b4
Denis Adler <firelizard@mail.ru>
2025-06-04 17:27:12 +0700

    создание веток по gitflow

--- src/masks.py
+++ src/masks.py
@@ -8,7 +8,7 @@ def get_mask_card_number(card_number: int) -> str:
 
     new_mask: str = (
         f"{str(card_number)[:4]} {str(card_number)[4:6]}** **** \
-{str(card_number)[8:12]}"
+{str(card_number)[-4:]}"
     )
 
     return new_mask
@@ -18,6 +18,6 @@ def get_mask_account(account_number: int) -> str:
     """Принимает номер аккаунта int, выдаёт замаскированный" str вида
     **ХХХХ"""
 
-    new_mask: str = f"**{str(account_number)[-5:-1]}"
+    new_mask: str = f"**{str(account_number)[-4:]}"
 
     return new_mask
